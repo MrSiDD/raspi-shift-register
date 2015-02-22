2a5f0798be6b93de5fa81c0ee043ac30e9f53719
Michaël Boué <michael.boue@wimionline.be>
2015-02-22 19:21:51 +0100

    Updated demo file

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
 from lib.ShiftRegister import ShiftRegister
+import time
 
 dataPin  = 24
 clockPin = 23
 latchPin = 26
 
 shiftRegister = ShiftRegister(dataPin, clockPin, latchPin)
+shiftRegister.setValue(0)
+time.sleep(1)
+shiftRegister.setValue(1)
+time.sleep(1)
+shiftRegister.setValue(0)
+time.sleep(1)
+shiftRegister.setValue(1)
+time.sleep(1)
+shiftRegister.setValue(0)
+time.sleep(1)
+shiftRegister.setValue(1)
+time.sleep(1)
+shiftRegister.setValue(0)
+time.sleep(1)
+shiftRegister.setValue(1)
+time.sleep(1)
