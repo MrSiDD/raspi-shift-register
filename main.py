7ee875f2302d928ea20635cb189c29edefe18e10
Michaël Boué <michael.boue@wimionline.be>
2015-02-22 19:24:03 +0100

    update

--- main.py
+++ main.py
@@ -6,19 +6,27 @@ clockPin = 23
 latchPin = 26
 
 shiftRegister = ShiftRegister(dataPin, clockPin, latchPin)
-shiftRegister.setValue(0)
-time.sleep(1)
-shiftRegister.setValue(1)
-time.sleep(1)
-shiftRegister.setValue(0)
-time.sleep(1)
-shiftRegister.setValue(1)
-time.sleep(1)
-shiftRegister.setValue(0)
-time.sleep(1)
-shiftRegister.setValue(1)
-time.sleep(1)
-shiftRegister.setValue(0)
-time.sleep(1)
-shiftRegister.setValue(1)
-time.sleep(1)
+
+try:
+	shiftRegister.setValue(0)
+	time.sleep(1)
+	shiftRegister.setValue(1)
+	time.sleep(1)
+	shiftRegister.setValue(0)
+	time.sleep(1)
+	shiftRegister.setValue(1)
+	time.sleep(1)
+	shiftRegister.setValue(0)
+	time.sleep(1)
+	shiftRegister.setValue(1)
+	time.sleep(1)
+	shiftRegister.setValue(0)
+	time.sleep(1)
+	shiftRegister.setValue(1)
+	time.sleep(1)
+
+except(KeyboardInterrupt, SystemExit)
+	print("Exit ...")
+
+finally:
+	GPIO.cleanup()
