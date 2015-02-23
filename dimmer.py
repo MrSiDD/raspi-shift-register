544a08c62b409c75dd3a6bcd3f7b18f4ced74306
Michaël Boué <michael.boue@wimionline.be>
2015-02-23 20:44:04 +0100

    Removed str()

--- dimmer.py
+++ dimmer.py
@@ -12,21 +12,19 @@ nullLed = [0,0,0,0,0,0,0,0]
 def generateRandomLedState():
     led = []
     for i in range(0,8):
-        led.append(str(random.randint(0,1)))
+        led.append(random.randint(0,1))
     return led
 
 try:
     while True:
         tempLed = generateRandomLedState()
         for i in range(0,10):
-            print(str(i) + " " + string.join(tempLed, ''))
             shiftRegister.setValue(tempLed)
             time.sleep(0.05 * i)
             shiftRegister.setValue(nullLed)
             time.sleep(0.05 * i)
 
         for i in reversed(range(0,10)):
-            print(str(i) + " " + string.join(tempLed, ''))
             shiftRegister.setValue(tempLed)
             time.sleep(0.05 * i)
             shiftRegister.setValue(nullLed)
